036dcb2d041da56d4b6affe58bd3caec6b6c2c05
peppa-pig6 <vishawjit20in@gmail.com>
2026-06-29 21:28:10 +0530

    Update

--- SmartHomeVoice.ino
+++ SmartHomeVoice.ino
@@ -18,42 +18,71 @@ D = Security ON   d = Security OFF
 
 void setup() {
   Serial.begin(9600);
+
   pinMode(lightPin, OUTPUT);
   pinMode(fanPin, OUTPUT);
   pinMode(securityPin, OUTPUT);
 
   doorServo.attach(servoPin);
   doorServo.write(0);
+
+  // IMPORTANT: clear any garbage serial data at startup
+  while (Serial.available()) {
+    Serial.read();
+  }
+
+  Serial.println("SYSTEM READY");
 }
 
 void loop() {
   if (Serial.available()) {
+
     char cmd = Serial.read();
 
     switch (cmd) {
+
       case 'A':
         digitalWrite(lightPin, HIGH);
+        Serial.println("LIGHT ON");
         break;
+
       case 'a':
         digitalWrite(lightPin, LOW);
+        Serial.println("LIGHT OFF");
         break;
+
       case 'B':
         digitalWrite(fanPin, HIGH);
+        Serial.println("FAN ON");
         break;
+
       case 'b':
         digitalWrite(fanPin, LOW);
+        Serial.println("FAN OFF");
         break;
+
       case 'C':
         doorServo.write(90);
+        Serial.println("DOOR OPEN");
         break;
+
       case 'c':
         doorServo.write(0);
+        Serial.println("DOOR CLOSE");
         break;
+
       case 'D':
         digitalWrite(securityPin, HIGH);
+        Serial.println("SECURITY ON");
         break;
+
       case 'd':
         digitalWrite(securityPin, LOW);
+        Serial.println("SECURITY OFF");
+        break;
+
+      default:
+        // ignore unknown commands safely
         break;
     }
   }
